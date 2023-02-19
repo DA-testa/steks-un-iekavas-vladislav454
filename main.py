7dcd6b24c47ed1ad0ad23916262d1b7cb6cf3585
vladislav454 <59504838+vladislav454@users.noreply.github.com>
2023-02-19 16:22:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# python3
-
 from collections import namedtuple
 
 Bracket = namedtuple("Bracket", ["char", "position"])
@@ -11,21 +9,36 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
-    for i, next in enumerate(text):
-        if next in "([{":
-            # Process opening bracket, write your code here
-            pass
-
-        if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
+    for i, char in enumerate(text):
+        if char in "([{":
+            # Process opening bracket
+            opening_brackets_stack.append(Bracket(char, i+1))
+        elif char in ")]}":
+            # Process closing bracket
+            if not opening_brackets_stack:
+                return i+1
+            top = opening_brackets_stack.pop()
+            if not are_matching(top.char, char):
+                return i+1
+    if opening_brackets_stack:
+        return opening_brackets_stack[0].position
+    else:
+        return "Success"
 
 
 def main():
-    text = input()
+    text = ""
+    input_type = input()
+    if "I" in input_type:
+        input_string = input()
+        input_string = input_string.replace("I\\r\\n", "")
+        text = input_string
+    elif input_type == "F":
+        file_path = input()
+        with open(file_path) as f:
+            text = f.read()
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
-
+    print(mismatch)
 
 if __name__ == "__main__":
     main()
